5e95e1514910a08d224d7482671e3284bbd67222
babk <b.s.142753@gmail.com>
2018-12-01 01:08:33 +0330

    made a logic for calculater to get rid of sympy

--- calc-bot.py
+++ calc-bot.py
@@ -1,90 +1,35 @@
 #!/.local/lib/python3.6
 # -*- coding: utf-8 -*-
-import telepot
-import time
+
 import urllib3
-import telebot
-import time
-import sympy
-
-#this part is for server settings 
-proxy_url = "SERVER:PORT" #your proxy server
-telepot.api._pools = {'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),}
-telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
+from telegram.ext import CommandHandler,Updater,MessageHandler,Filters
+import logging
 
 
+logging.basicConfig(format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',level = logging.INFO)
 
-def listener(messages):
-    """
-    When new messages arrive TeleBot will call this function.
-    """
-    for m in messages:
-        if m.content_type == 'text':
-            print(m.text) # prints the sent message to the console
+logger = logging.getLogger(__name__)
 
 
-bot = telebot.TeleBot('<TOKEN>') #your <TOKEN> goes here
-bot.set_update_listener(listener)  # register listener
+def start_command(bot, Update):
+    print(":)!")
+    update.message.reply_text('it is working!!!')
 
 
-@bot.message_handler(commands=['help']) #Discribes how to use and what it is doing
-def help_command(m):
-	bot.send_message(m.chat.id, '''
-    it is  calcroid you can start by typing /calculate and get out with /exit command
-    ''')
+def main():
+    updater = Updater("679603790:AAGHUr0nnh6Ki7CkfCyOvRR-cU5uxJATsIc")
+
+    d = updater.dispatcher
+
+    d.add_handler(CommandHandler("start",start_command))
+
+    updater.start_polling()
+
+    updater.idle
 
 
-@bot.message_handler(commands=['calculate'])#turn on the calculation function
-def calc_start(m):
-    bot.reply_to(m, "ok")
-    bot.send_message(m.chat.id, "what you want me to do?")
-    global comm
-    comm = 1
-    return
-
-
-@bot.message_handler(commands=['exit'])#turn off the functions
-def exit_command(m) :
-    global comm
-    comm = 0
-    bot.send_message(m.chat.id, "thanks for using my bot")
-    return
-
-
-def error_type(m) :
-    bot.send_message(m.chat.id, "error :(")#any other kind of not defined texts will lead to error (temporary)
-    return
-
-
-@bot.message_handler(func=lambda message: True, content_types = ['text']) #the main calculation function
-def calc_msg(m):  
-        try :
-            if comm == 1 :
-                s = str(m.text)
-                group = ['0','1','2','3','4','5','6','7','8','9','+','-','*','/','^','(',')','%','!'] #all acceptble characters for calculating
-                for ele in range(0,len(s)) :
-                    if s[ele] in group :
-                        b = True
-                    else :
-                        b = False
-                        break
-                if b == True :
-                    anw = sympy.sympify(m.text)
-                    bot.reply_to(m, anw)
-                elif b == False :
-                    error_type(m)
-            else:
-                error_type(m)
-        except NameError :
-            bot.send_message(m.chat.id, "use /help")
-            error_type(m)
-
-
-print ('Listening ...')#just to see if it connects
-
-bot.polling()#starting bot
-
-
+if __name__ == "__main__":
+    main()
 # Keep the program running.
-while 1:
-    time.sleep(10)
+#while 1:
+ #   time.sleep(10)
---
+++ calculator_logic.py
@@ -0,0 +1,66 @@
+def main():
+    while(True):
+        exp = input()
+        exp = calculate(exp)        
+        print(exp)
+
+
+def calculate(exp):#they are in order
+    while ('!' in exp):
+        cord = exp.index('!')
+        i = cord
+        while (i != 0 and exp[i-1].isdigit() == True):
+            i -= 1
+        num = int(exp[i:cord])
+        exp = exp.replace(exp[i:cord+1],str(fac(num)))
+    while ('(' in exp):
+        exp = parent(exp)
+    while ('*' in exp):
+        exp = operator(exp,'*')
+    while ('/' in exp):
+        exp = operator(exp,'/')
+    while ('+' in exp):
+        exp = operator(exp,'+')
+    while ('-' in exp):
+        exp = operator(exp,'-')
+    return exp
+
+
+def parent(exp_tmp): #runs until there are parenthesis
+    cord_s = exp_tmp.index('(')
+    cord_e = len(exp_tmp) - exp_tmp[::-1].index(')') - 1
+    anw = calculate(exp_tmp[cord_s + 1:cord_e])
+    exp_tmp = exp_tmp.replace(exp_tmp[cord_s:cord_e + 1],str(anw))
+    return exp_tmp
+
+def operator(exp_tmp,opr):#operators (+, -, /, *)
+    cord = exp_tmp.index(opr)
+    i = cord
+    j = cord
+    while (i != 0 and exp_tmp[i-1].isdigit() == True):
+        i -= 1
+    while (j != len(exp_tmp) - 1 and exp_tmp[j+1].isdigit() == True):
+        j += 1
+    old = exp_tmp[i:j+1]
+    x,y = exp_tmp[i:j+1].split(opr)
+    if (opr == '*'):
+        multi = int(x) * int(y)
+    elif (opr == '/'):
+        multi = int(x) / int(y)
+    elif (opr == '+'):
+        multi = int(x) + int(y)
+    elif (opr == '-'):
+        multi = int(x) - int(y)
+    exp_tmp = exp_tmp.replace(old,str(multi))
+    return exp_tmp
+
+
+def fac(num):#factoriel algorithm written with recursion
+    if (num == 0):
+        return 1
+    else:
+        return num * fac(num-1)
+
+
+if __name__ == "__main__":
+    main()
